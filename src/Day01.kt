a6e7f81fa85d3712b431bec4df735785ba654ea3
Radek Novotný <111576015+novotnyradekcz@users.noreply.github.com>
2022-12-21 16:45:58 +0100

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,44 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var calories = 0
+        val maxima = mutableListOf<Int>()
+        for (line in input) {
+            if (line == "") {
+                maxima.add(calories)
+                calories = 0
+            } else {
+                calories += line.toInt()
+            }
+        }
+        maxima.add(calories)
+        return maxima.max()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var calories = 0
+        val maxima = mutableListOf<Int>()
+        for (line in input) {
+            if (line == "") {
+                maxima.add(calories)
+                calories = 0
+            } else {
+                calories += line.toInt()
+            }
+        }
+        maxima.add(calories)
+        var topThree = 0
+        topThree += maxima.max()   // highest
+        maxima.remove(maxima.max())
+        topThree += maxima.max()   // second highest
+        maxima.remove(maxima.max())
+        topThree += maxima.max()   // third highest
+        return topThree
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     part1(input).println()
